42a148122a12762f69f195a7c018d78684c816ae
Documenter.jl <documenter@juliadocs.github.io>
2024-12-28 12:01:53 +0000

    build based on 5dd23e0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectralWaves","category":"page"},{"location":"#SpectralWaves","page":"Home","title":"SpectralWaves","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectralWaves.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectralWaves]","category":"page"}]
+}
