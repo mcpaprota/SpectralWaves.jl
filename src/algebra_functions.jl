0a20474df6b475aa8f9b9c761b7f96253eb89156
mcpaprota <mapap@ibwpan.gda.pl>
2024-12-30 21:56:15 +0100

    definition of types for convolutions

--- src/algebra_functions.jl
+++ src/algebra_functions.jl
@@ -1,21 +1,21 @@
 """
-    convolve(a, b) or a * b
+    convolve(a::Vector{T}, b::Vector{T}) where T
 
 Compute direct convolution of two vectors `a` and `b`.
 
 # Examples
 ```julia-repl
-julia> a = [1, 2]; b = [1, 1]; convolve(a,b)
+julia> a = [1, 2]; b = [1, 1]; convolve(a, b)
 3-element Vector{Int64}:
  1
  3
  2
 ```
 """
-function convolve(a, b)
+function convolve(a::Vector{T}, b::Vector{T}) where T<:Number
     n = length(a)
     m = length(b)
-    c = zero([a; b[1:end-1]])
+    c = zeros(T, n + m - 1)
     for i in 1:n
         for j in 1:m
             c[i+j-1] += a[i] * b[j]
@@ -24,10 +24,10 @@ function convolve(a, b)
     return c
 end
 
-Base.:*(a, b) = convolve(a, b) # infix version of convolution
+Base.:*(a::Vector{<:Number}, b::Vector{<:Number}) = convolve(a, b) # infix version of convolve
 
 """
-    convolution_power(a, n) or a ^ n
+    convolution_power(a::Vector{T}, n::Integer) where T<:Number
 
 Compute convolution `n`-th power of vector `a`.
 
@@ -44,8 +44,7 @@ julia> a = [1, 2, 3]; convolution_power(a, 3)
  27
 ```
 """
-function convolution_power(a, n::Integer)
-    m = length(a)
+function convolution_power(a::Vector{T}, n::Integer) where T<:Number
     if n == 0
         return a[1] * 0 + 1
     elseif n == 1
@@ -55,12 +54,12 @@ function convolution_power(a, n::Integer)
     end
 end
 
-Base.:^(a, n) = convolution_power(a, n) # infix version of convolution n-th power
+Base.:^(a::Vector{<:Number}, n::Integer) = convolution_power(a, n) # infix version of convolution n-th power
 
 """
-    toeplitz(a)
+    toeplitz(a::Vector{T}) where T
 
-Transform vector `a` to Toeplitz matrix.
+Transform vector `a` to a Toeplitz matrix.
 
 # Examples
 ```julia-repl
@@ -71,9 +70,9 @@ julia> a = [1, 2, 3, 4, 5]; toeplitz(a)
  5  4  3
 ```
 """
-function toeplitz(a)
+function toeplitz(a::Vector{T}) where T<:Number
     n = Int((length(a) - 1) ÷ 2)
-    B = similar(a, n + 1, n + 1) * 0
+    B = zeros(T, n+1, n+1)
     for i in 0:n, j in 0:n
         B[i+1, j+1] = a[i-j+n+1]
     end
